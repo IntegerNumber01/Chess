b8caf296b4dcd3a9305cfe7448702ecee27c63c5
Integer Number <124754713+IntegerNumber01@users.noreply.github.com>
2024-02-04 09:34:40 -0800

    Create pieces.py
    Update line 17 with directory

---
+++ src/pieces.py
@@ -0,0 +1,38 @@
+import pygame
+
+
+class Pieces:
+    def __init__(self, screen, size):
+        self.images = self.load_images()
+        self.resize_images(size)
+        self.screen = screen
+
+    def load_images(self):
+        names = ['K', 'Q', 'R', 'N', 'B', 'P', 'k', 'q', 'r', 'n', 'b', 'p']
+        files = ['WK', 'WQ', 'WR', 'WN', 'WB', 'WP',
+                 'Bk', 'Bq', 'Br', 'Bn', 'Bb', 'Bp']
+        images = {}
+
+        for i in range(len(names)):
+            images[names[i]] = pygame.image.load('directory' + files[i] + '.png')
+
+        return images
+
+    def resize_images(self, size):
+        for key, value in self.images.items():
+            new_width = int(value.get_width() * size)
+            new_height = int(value.get_height() * size)
+
+            self.images[key] = pygame.transform.smoothscale(value, (new_width,
+                                                                    new_height)
+                                                            )
+
+    def render(self, piece, coordinates, transparency):
+        sprite = self.images[piece]
+
+        if transparency is not None:
+            copy = sprite.copy()
+            copy.set_alpha(transparency)
+            self.screen.blit(copy, sprite.get_rect(center=coordinates))
+        else:
+            self.screen.blit(sprite, sprite.get_rect(center=coordinates))
